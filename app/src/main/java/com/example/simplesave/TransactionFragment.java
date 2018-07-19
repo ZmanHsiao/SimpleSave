719db3ec8edeb588f564977a5860bba365bcd6b4
Joshua Cao <caoj11@uci.edu>
2018-07-18 23:39:08 -0700

    transaction stuff that i broke

--- app/src/main/java/com/example/simplesave/TransactionFragment.java
+++ app/src/main/java/com/example/simplesave/TransactionFragment.java
@@ -3,15 +3,17 @@ package com.example.simplesave;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import com.google.firebase.Timestamp;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -19,76 +21,22 @@ public class TransactionFragment extends Fragment {
 
 
     private Spinner dropdown;
-    private Button changeDate;
-    private Timestamp date;
-
-    public TransactionFragment() {
-
-
-    }
+    List<Transaction> transList;
+    RecyclerView recyclerView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transactions, container, false);
-        TextView trans = (TextView)view.findViewById(R.id.transactions);
 
-        dropdown = (Spinner) view.findViewById(R.id.dayDropdown);
-        String[] items = new String[MainActivity.budgetplan.getTotalDays() - MainActivity.budgetplan.getDaysLeft() + 1];
-        for (int i = 0; i < items.length; i++) {
-            items[i] = Integer.toString(i+1);
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, items);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown.setAdapter(adapter);
-
-        String text = "";
-        for (int i = 0; i < MainActivity.budgetplan.getTransactions().size(); i++) {
-                text += MainActivity.budgetplan.getTransactions().get(i).getTimestamp().toDate()
-                        + ") " + MainActivity.budgetplan.getTransactions().get(i).getName() +
-                        "  $" + MainActivity.budgetplan.getTransactions().get(i).getPrice() + "\n";
-        }
-        trans.setText(text);
-
-        Button addMoney = (Button) view.findViewById(R.id.specDay);
-        addMoney.setOnClickListener(addCheckDayListener);
-
+        recyclerView = (RecyclerView) view.findViewById(R.id.recylcerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        transList = MainActivity.budgetplan.getTransactions();
+        TransactionAdapter adapter = new TransactionAdapter(getActivity(), transList);
+        recyclerView.setAdapter(adapter);
         return view;
     }
 
-    private View.OnClickListener addCheckDayListener = new View.OnClickListener() {
-        public void onClick(View view) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            View mView = getLayoutInflater().inflate(R.layout.check_day_dialog, null);
-            TextView dayCount = (TextView) mView.findViewById((R.id.dayCount));
-            TextView transactions = (TextView) mView.findViewById(R.id.dayTransactions);
-
-
-            int selected = Integer.parseInt(dropdown.getSelectedItem().toString());
-            dayCount.setText("Day " + selected);
-            String text = "";
-            List<Transaction> dayTransactions = MainActivity.budgetplan.getTransactions();
-            for ( int i = 0; i < dayTransactions.size(); i++) {
-                //if (dayTransactions.get(i).getDate() == selected - 1) {
-                    text += dayTransactions.get(i).getName() + "   $" + dayTransactions.get(i).getPrice() + "\n";
-                //}
-            }
-            transactions.setText(text);
-
-
-            Button button = (Button) mView.findViewById(R.id.back);
-            builder.setView(mView);
-            builder.create();
-            final AlertDialog display = builder.show();
-
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    display.dismiss();
-                }
-            });
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
