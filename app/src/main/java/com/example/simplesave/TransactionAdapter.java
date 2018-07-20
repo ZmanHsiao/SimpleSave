a3853bf4b836fb487bd636529ea373ce141409c7
ZmanH <ZmanHsiao@gmail.com>
2018-07-19 23:42:36 -0700

    editable transactions

--- app/src/main/java/com/example/simplesave/TransactionAdapter.java
+++ app/src/main/java/com/example/simplesave/TransactionAdapter.java
@@ -1,5 +1,7 @@
 package com.example.simplesave;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
@@ -7,16 +9,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
-
-
 import java.util.List;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
 
     private Context mCtx;
     private List <Transaction> transList;
+    private LayoutInflater inflater;
 
     public TransactionAdapter(Context mCtx, List<Transaction> transList) {
         this.mCtx = mCtx;
@@ -26,7 +31,7 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @Override
     public TransactionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflating and returning our view holder
-        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        inflater = LayoutInflater.from(mCtx);
         View view = inflater.inflate(R.layout.cards_layout, null);
         return new TransactionViewHolder(view);
     }
@@ -80,7 +85,56 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             category = itemView.findViewById(R.id.category);
             price = itemView.findViewById(R.id.price);
             imageView = itemView.findViewById(R.id.imageView);
+
+            itemView.setOnClickListener(editTransListener);
         }
+
+        private View.OnClickListener editTransListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Transaction t = transList.get(getAdapterPosition());
+                AlertDialog.Builder builder = new AlertDialog.Builder(mCtx);
+                View mView = inflater.inflate(R.layout.edit_transaction_dialog, null);
+                final EditText price = (EditText) mView.findViewById(R.id.price);
+                final EditText title = (EditText) mView.findViewById(R.id.title);
+                builder.setView(mView);
+                builder.create();
+                final AlertDialog display = builder.show();
+
+                final Spinner dropdown = (Spinner) mView.findViewById(R.id.categoryDropdown);
+                final ArrayAdapter<String> adapter = new ArrayAdapter<String>(mCtx, android.R.layout.simple_list_item_1,
+                        MainActivity.budgetplan.getCategories().keySet().toArray(new String[0]));
+                dropdown.setAdapter(adapter);
+
+                Button edit = (Button) mView.findViewById(R.id.edit);
+                Button delete = (Button) mView.findViewById(R.id.delete);
+
+                edit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String category = dropdown.getSelectedItem().toString();
+                        String name = title.getText().toString();
+                        float val = Float.valueOf(price.getText().toString());
+                        t.setName(name);
+                        t.setCategory(category);
+                        t.setPrice(val);
+                        notifyDataSetChanged();
+                        display.dismiss();
+                    }
+                });
+
+                delete.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        transList.remove(getAdapterPosition());
+                        notifyItemRemoved(getAdapterPosition());
+                        notifyItemRangeChanged(getAdapterPosition(), transList.size());
+                        MainActivity.budgetplan.getTransactions().remove(t);
+                        display.dismiss();
+                    }
+                });
+            }
+        };
     }
 
 }
--- app/src/main/java/com/example/simplesave/TransactionFragment.java
+++ app/src/main/java/com/example/simplesave/TransactionFragment.java
@@ -3,96 +3,76 @@ package com.example.simplesave;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.Timestamp;
+import com.google.android.gms.common.util.ArrayUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionFragment extends Fragment {
 
 
-    private Spinner dropdown;
-    private Button changeDate;
-    private Timestamp date;
-
-    public TransactionFragment() {
-
-
-    }
+    private RecyclerView recyclerView;
+    List<Transaction> transList;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transactions, container, false);
-        TextView trans = (TextView)view.findViewById(R.id.transactions);
 
-        dropdown = (Spinner) view.findViewById(R.id.dayDropdown);
-        String[] items = new String[MainActivity.budgetplan.getTotalDays() - MainActivity.budgetplan.getDaysLeft() + 1];
-        for (int i = 0; i < items.length; i++) {
-            items[i] = Integer.toString(i+1);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recylcerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        String[] categories = MainActivity.budgetplan.getCategories().keySet().toArray(new String[0]);
+        String[] array = new String[categories.length + 1];
+        array[0] = "Transactions";
+        for (int i = 1; i < array.length; i++) {
+            array[i] = categories[i-1];
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, items);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown.setAdapter(adapter);
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, array);
+        spinner.setAdapter(spinAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                String selected = parent.getItemAtPosition(i).toString();
+                transList = MainActivity.budgetplan.getTransactions();
+                if (!selected.equals("Transactions")) {
+                    List<Transaction> trans = new ArrayList<>();
+                    for (Transaction t: transList) {
+                        if (t.getCategory().equals(selected))
+                            trans.add(t);
+                    }
+                    transList = trans;
+                }
+                TransactionAdapter adapter = new TransactionAdapter(getContext(), transList);
+                recyclerView.setAdapter(adapter);
+            }
 
-        String text = "";
-        for (int i = 0; i < MainActivity.budgetplan.getTransactions().size(); i++) {
-                text += MainActivity.budgetplan.getTransactions().get(i).getTimestamp().toDate()
-                        + ") " + MainActivity.budgetplan.getTransactions().get(i).getName() +
-                        "  $" + MainActivity.budgetplan.getTransactions().get(i).getPrice() + "\n";
-        }
-        trans.setText(text);
-
-        Button addMoney = (Button) view.findViewById(R.id.specDay);
-        addMoney.setOnClickListener(addCheckDayListener);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
 
         return view;
     }
-
-    private View.OnClickListener addCheckDayListener = new View.OnClickListener() {
-        public void onClick(View view) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            View mView = getLayoutInflater().inflate(R.layout.check_day_dialog, null);
-            TextView dayCount = (TextView) mView.findViewById((R.id.dayCount));
-            TextView transactions = (TextView) mView.findViewById(R.id.dayTransactions);
-
-
-            int selected = Integer.parseInt(dropdown.getSelectedItem().toString());
-            dayCount.setText("Day " + selected);
-            String text = "";
-            List<Transaction> dayTransactions = MainActivity.budgetplan.getTransactions();
-            for ( int i = 0; i < dayTransactions.size(); i++) {
-                //if (dayTransactions.get(i).getDate() == selected - 1) {
-                    text += dayTransactions.get(i).getName() + "   $" + dayTransactions.get(i).getPrice() + "\n";
-                //}
-            }
-            transactions.setText(text);
-
-
-            Button button = (Button) mView.findViewById(R.id.back);
-            builder.setView(mView);
-            builder.create();
-            final AlertDialog display = builder.show();
-
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    display.dismiss();
-                }
-            });
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
 
 }
